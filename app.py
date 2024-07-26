00301106400caa5dd1ef4df56f484ac8fc8c8b3f
pannyumon <pannyum@gmail.com>
2024-07-26 14:54:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 # Create an instance of the Flask class
 app = Flask(__name__)
 
 # Define a route for the root URL
 @app.route('/')
-def hello_world():
-    greeting = "World"
-    return f'Hello, {greeting}!'
-
-# Run the Flask application if this script is executed
-if __name__ == "__main__":
-    app.run()
+def index():
+    greeting = "ハローワールド！"
+    return render_template("index.html", greeting=greeting)
